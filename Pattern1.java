f8ba37cfadbb703c06158d01b214b5cea0028e7c
dnyandaclasses <53405524+dnyandaclasses@users.noreply.github.com>
2021-04-11 23:30:49 +0530

    copyrights

--- Pattern1.java
+++ Pattern1.java
@@ -1,3 +1,20 @@
+/*   Author : Dnyanda Engineering Classes
+     Description:     
+	1
+	12
+	123
+	1234
+	12345
+	123456
+	1234567
+	123456
+	12345
+	1234
+	123
+	12
+	1
+
+*/
 class Pattern1
 {
 	public static void main(String args[])
